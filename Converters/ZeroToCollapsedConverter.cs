bc463936d76b5b1539f0b03a080fbeaeb7ed837e
gefeeHome <megamisha41521@gmail.com>
2026-04-21 21:16:44 +0300

    ZeroToCollapsedConverter add

---
+++ Converters/ZeroToCollapsedConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Converters
+{
+    public class ZeroToCollapsedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // Если значение 0 или меньше — скрываем, иначе показываем
+            return (int)value == 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
