8ce8e3aab18b33afe66df4344b5bb07946090f09
gefeeHome <megamisha41521@gmail.com>
2026-04-28 20:06:47 +0300

    add PeriodToStyleConverter

---
+++ Converters/PeriodToStyleConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using TimeTracker.ViewModels;
+
+namespace TimeTracker.Converters;
+
+public class PeriodToStyleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is int current && parameter is string targetStr)
+        {
+            if (int.TryParse(targetStr, out int target))
+            {
+                return current == target
+                    ? Application.Current.Resources["ActivePeriodButtonStyle"]
+                    : Application.Current.Resources["InactivePeriodButtonStyle"];
+            }
+        }
+
+        return Application.Current.Resources["InactivePeriodButtonStyle"];
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+        => throw new NotImplementedException();
+}
